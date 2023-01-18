4f38c945fb49d561b3fcc5a3b5d391ea93662d64
Renzo <renzol2@illinois.edu>
2023-01-18 02:05:32 -0600

    Add cookiecutter template for distortion

---
+++ distortion/README.md
@@ -0,0 +1,9 @@
+# Distortion
+
+## Building
+
+After installing [Rust](https://rustup.rs/), you can compile Distortion as follows:
+
+```shell
+cargo xtask bundle distortion --release
+```
---
+++ distortion/src/lib.rs
@@ -0,0 +1,148 @@
+use nih_plug::prelude::*;
+use std::sync::Arc;
+
+// This is a shortened version of the gain example with most comments removed, check out
+// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
+// started
+
+struct Distortion {
+    params: Arc<DistortionParams>,
+}
+
+#[derive(Params)]
+struct DistortionParams {
+    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
+    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
+    /// parameters are exposed to the host in the same order they were defined. In this case, this
+    /// gain parameter is stored as linear gain while the values are displayed in decibels.
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for Distortion {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(DistortionParams::default()),
+        }
+    }
+}
+
+impl Default for DistortionParams {
+    fn default() -> Self {
+        Self {
+            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
+            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
+            // as decibels is easier to work with, but requires a conversion for every sample.
+            gain: FloatParam::new(
+                "Gain",
+                util::db_to_gain(0.0),
+                FloatRange::Skewed {
+                    min: util::db_to_gain(-30.0),
+                    max: util::db_to_gain(30.0),
+                    // This makes the range appear as if it was linear when displaying the values as
+                    // decibels
+                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                },
+            )
+            // Because the gain parameter is stored as linear gain instead of storing the value as
+            // decibels, we need logarithmic smoothing
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
+            .with_unit(" dB")
+            // There are many predefined formatters we can use here. If the gain was stored as
+            // decibels instead of as a linear gain value, we could have also used the
+            // `.with_step_size(0.1)` function to get internal rounding.
+            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+        }
+    }
+}
+
+impl Plugin for Distortion {
+    const NAME: &'static str = "Distortion";
+    const VENDOR: &'static str = "Renzo Ledesma";
+    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
+    const EMAIL: &'static str = "renzol2@illinois.edu";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    const DEFAULT_INPUT_CHANNELS: u32 = 2;
+    const DEFAULT_OUTPUT_CHANNELS: u32 = 2;
+
+    const DEFAULT_AUX_INPUTS: Option<AuxiliaryIOConfig> = None;
+    const DEFAULT_AUX_OUTPUTS: Option<AuxiliaryIOConfig> = None;
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    // More advanced plugins can use this to run expensive background tasks. See the field's
+    // documentation for more information. `()` means that the plugin does not have any background
+    // tasks.
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn accepts_bus_config(&self, config: &BusConfig) -> bool {
+        // This works with any symmetrical IO layout
+        config.num_input_channels == config.num_output_channels && config.num_input_channels > 0
+    }
+
+    fn initialize(
+        &mut self,
+        _bus_config: &BusConfig,
+        _buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        // Resize buffers and perform other potentially expensive initialization operations here.
+        // The `reset()` function is always called right after this function. You can remove this
+        // function if you do not need it.
+        true
+    }
+
+    fn reset(&mut self) {
+        // Reset buffers and envelopes here. This can be called from the audio thread and may not
+        // allocate. You can remove this function if you do not need it.
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for channel_samples in buffer.iter_samples() {
+            // Smoothing is optionally built into the parameters themselves
+            let gain = self.params.gain.smoothed.next();
+
+            for sample in channel_samples {
+                *sample *= gain;
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+}
+
+impl ClapPlugin for Distortion {
+    const CLAP_ID: &'static str = "com.your-domain.distortion";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("Algorithms of nonlinear systems for distortion effects");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = None;
+
+    // Don't forget to change these features
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect, ClapFeature::Stereo];
+}
+
+impl Vst3Plugin for Distortion {
+    const VST3_CLASS_ID: [u8; 16] = *b"renzol2_distortn";
+
+    // And don't forget to change these categories, see the docstring on `VST3_CATEGORIES` for more
+    // information
+    const VST3_CATEGORIES: &'static str = "Fx|Dynamics";
+}
+
+nih_export_clap!(Distortion);
+nih_export_vst3!(Distortion);
