1693527df95f69e84ccf25c7bdcad32861998f96
Renzo <renzol2@illinois.edu>
2023-02-28 01:15:51 -0600

    Try implementing LPF

--- equalizer/src/lib.rs
+++ equalizer/src/lib.rs
@@ -1,8 +1,67 @@
 use nih_plug::prelude::*;
 use std::sync::Arc;
 
+struct BiquadFilter {
+    // Coefficients
+    a0: f32,
+    a1: f32,
+    a2: f32,
+    b0: f32,
+    b1: f32,
+    b2: f32,
+
+    // Unit delays
+    z1: f32,
+    z2: f32,
+}
+
+impl BiquadFilter {
+    fn new(frequency: f32) -> BiquadFilter {
+        BiquadFilter::get_lowpass_filter_coefficients(frequency, 1.0)
+    }
+
+    fn get_lowpass_filter_coefficients(frequency: f32, q: f32) -> BiquadFilter {
+        let alpha = (frequency.sin()) / (2.0 * q);
+        let b0 = (1.0 - frequency.cos()) / 2.0;
+        let b1 = 1.0 - frequency.cos();
+        let b2 = (1.0 - frequency.cos()) / 2.0;
+        let a0 = 1.0 + alpha;
+        let a1 = -2.0 * frequency.cos();
+        let a2 = 1.0 - alpha;
+        BiquadFilter {
+            a0,
+            a1,
+            a2,
+            b0,
+            b1,
+            b2,
+            z1: 0.0,
+            z2: 0.0,
+        }
+    }
+
+    fn update_filter_coefficients(&mut self, frequency: f32, q: f32) {
+        let alpha = (frequency.sin()) / (2.0 * q);
+        self.b0 = (1.0 - frequency.cos()) / 2.0;
+        self.b1 = 1.0 - frequency.cos();
+        self.b2 = (1.0 - frequency.cos()) / 2.0;
+        self.a0 = 1.0 + alpha;
+        self.a1 = -2.0 * frequency.cos();
+        self.a2 = 1.0 - alpha;
+    }
+
+    fn process(&mut self, input: f32) -> f32 {
+        let output = input * self.a0 + self.z1;
+        self.z1 = input * self.a1 + self.z2 - self.b1 * output;
+        self.z2 = input * self.a2 - self.b2 * output;
+        return output;
+    }
+}
+
 pub struct Equalizer {
     params: Arc<EqualizerParams>,
+    biquad: BiquadFilter,
+    should_update_filter: Arc<std::sync::atomic::AtomicBool>,
 }
 
 #[derive(Params)]
@@ -13,12 +72,24 @@ struct EqualizerParams {
     /// gain parameter is stored as linear gain while the values are displayed in decibels.
     #[id = "gain"]
     pub gain: FloatParam,
+
+    #[id = "cutoff-frequency"]
+    pub cutoff_frequency: FloatParam,
+
+    #[id = "q"]
+    pub q: FloatParam,
 }
 
 impl Default for Equalizer {
     fn default() -> Self {
         Self {
             params: Arc::new(EqualizerParams::default()),
+            biquad: BiquadFilter::new(
+                EqualizerParams::default()
+                    .cutoff_frequency
+                    .default_normalized_value(),
+            ),
+            should_update_filter: Arc::new(std::sync::atomic::AtomicBool::new(false)),
         }
     }
 }
@@ -39,12 +110,34 @@ impl Default for EqualizerParams {
             .with_unit(" dB")
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+
+            cutoff_frequency: FloatParam::new(
+                "Cutoff",
+                1_000.0,
+                FloatRange::Skewed {
+                    min: 20.0,
+                    max: 20_000.0,
+                    factor: FloatRange::skew_factor(2.0),
+                },
+            )
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
+            .with_unit(" Hz"),
+
+            q: FloatParam::new(
+                "Q",
+                1.0,
+                FloatRange::Linear {
+                    min: 0.1,
+                    max: 30.0,
+                },
+            )
+            .with_smoother(SmoothingStyle::Logarithmic(50.0)),
         }
     }
 }
 
 impl Plugin for Equalizer {
-    const NAME: &'static str = "Equalizer";
+    const NAME: &'static str = "Equalizer v0.0.2";
     const VENDOR: &'static str = "Renzo Ledesma";
     const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
     const EMAIL: &'static str = "renzol2@illinois.edu";
@@ -61,8 +154,7 @@ impl Plugin for Equalizer {
         aux_output_ports: &[],
 
         // Individual ports and the layout as a whole can be named here. By default these names
-        // are generated as needed. This layout will be called 'Stereo', while a layout with
-        // only one input and output channel would be called 'Mono'.
+        // are generated as needed. This layout will be called 'Stereo', while a layout with // only one input and output channel would be called 'Mono'.
         names: PortNames::const_default(),
     }];
 
@@ -95,12 +187,29 @@ impl Plugin for Equalizer {
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
+        // Check if we should update filter coefficients
+        if self
+            .should_update_filter
+            .compare_exchange(
+                true,
+                false,
+                std::sync::atomic::Ordering::Acquire,
+                std::sync::atomic::Ordering::Relaxed,
+            )
+            .is_ok()
+        {
+            let frequency = self.params.cutoff_frequency.smoothed.next();
+            let q = self.params.q.smoothed.next();
+            self.biquad.update_filter_coefficients(frequency, q);
+        }
+
         for channel_samples in buffer.iter_samples() {
             // Smoothing is optionally built into the parameters themselves
             let gain = self.params.gain.smoothed.next();
 
             for sample in channel_samples {
-                *sample *= gain;
+                let processed = self.biquad.process(*sample);
+                *sample *= processed * gain;
             }
         }
 
