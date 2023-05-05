783635257207ce5d8ec676358465ecb8b6da8f60
Renzo <renzol2@illinois.edu>
2023-05-05 01:06:02 -0500

    Create boilerplate for chorus effect

---
+++ chorus/README.md
@@ -0,0 +1,9 @@
+# Chorus
+
+## Building
+
+After installing [Rust](https://rustup.rs/), you can compile Chorus as follows:
+
+```shell
+cargo xtask bundle chorus --release
+```
---
+++ chorus/src/lib.rs
@@ -0,0 +1,122 @@
+use nih_plug::prelude::*;
+use std::sync::Arc;
+
+pub struct Chorus {
+    params: Arc<ChorusParams>,
+}
+
+#[derive(Params)]
+struct ChorusParams {
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for Chorus {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(ChorusParams::default()),
+        }
+    }
+}
+
+impl Default for ChorusParams {
+    fn default() -> Self {
+        Self {
+            gain: FloatParam::new(
+                "Gain",
+                util::db_to_gain(0.0),
+                FloatRange::Skewed {
+                    min: util::db_to_gain(-30.0),
+                    max: util::db_to_gain(30.0),
+                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                },
+            )
+            .with_smoother(SmoothingStyle::Logarithmic(50.0))
+            .with_unit(" dB")
+            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+        }
+    }
+}
+
+impl Plugin for Chorus {
+    const NAME: &'static str = "Chorus";
+    const VENDOR: &'static str = "Renzo Ledesma";
+    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
+    const EMAIL: &'static str = "renzol2@illinois.edu";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: NonZeroU32::new(2),
+        main_output_channels: NonZeroU32::new(2),
+
+        aux_input_ports: &[],
+        aux_output_ports: &[],
+
+        names: PortNames::const_default(),
+    }];
+
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    type SysExMessage = ();
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        _buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        true
+    }
+
+    fn reset(&mut self) {
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for channel_samples in buffer.iter_samples() {
+            let gain = self.params.gain.smoothed.next();
+
+            for sample in channel_samples {
+                *sample *= gain;
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+}
+
+impl ClapPlugin for Chorus {
+    const CLAP_ID: &'static str = "com.your-domain.chorus";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("A traditional chorus effect");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = None;
+
+    // Don't forget to change these features
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect, ClapFeature::Stereo];
+}
+
+impl Vst3Plugin for Chorus {
+    const VST3_CLASS_ID: [u8; 16] = *b"renzol2___chorus";
+
+    // And also don't forget to change these categories
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Fx, Vst3SubCategory::Dynamics];
+}
+
+nih_export_clap!(Chorus);
+nih_export_vst3!(Chorus);
